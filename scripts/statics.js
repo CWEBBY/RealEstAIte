604d7fd92d60e13e763c72b5a2c86c1b07497b4d
ModuloCW <chrjohweb@gmail.com>
2019-11-11 18:36:05 +1100

    Delete statics.js

--- scripts/statics.js
+++
@@ -1,8 +0,0 @@
-const PORT = 8080;
-const SERVER_IP = "127.0.0.1";
-const HTTP_PUBLIC_DIR = '../resources/pages/pageresources';
-const EMAIL_ADDRESS = "realestaiteproject@gmail.com";
-const EMAIL_PASSWORD = "INFT6900";
-module.exports = {
-  PORT, HTTP_PUBLIC_DIR, SERVER_IP, EMAIL_ADDRESS, EMAIL_PASSWORD
-}
